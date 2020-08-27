42edee14ae3c6fa87c18a64956b3709bb3f3e142
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-08-26 23:27:35 -0400

    Added build script
    - This is the script that I use to do builds

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+PROJ_ROOT="$(PWD)"
+
+cd "$PROJ_ROOT/CentOS"
+packer validate -syntax-only centos7.json
+packer validate -syntax-only centos8.json
+
+cd "$PROJ_ROOT/Debian"
+packer validate -syntax-only debian-buster64.json
+packer validate -syntax-only debian-stretch64.json
+
+cd "$PROJ_ROOT/Ubuntu"
+packer validate -syntax-only ubuntu-bionic64.json
+packer validate -syntax-only ubuntu-xenial64.json
+packer validate -syntax-only ubuntu-focal64.json
+
+cd "$PROJ_ROOT/CentOS"
+packer build -except vsphere-iso -except qemu -except virtualbox-ovf -except vmware-vmx centos7.json
+packer build -only qemu centos7.json
+packer build -only virtualbox-iso -only vmware-iso centos8.json
+packer build -except vsphere-iso -except qemu -except virtualbox-ovf -except vmware-vmx centos8.json
+packer build -only qemu centos8.json
+
+cd "$PROJ_ROOT/Debian"
+packer build -except vsphere-iso -except qemu -except virtualbox-ovf -except vmware-vmx debian-buster64.json
+packer build -only qemu debian-buster64.json
+packer build -except vsphere-iso -except qemu -except virtualbox-ovf -except vmware-vmx debian-stretch64.json
+packer build -only qemu debian-stretch64.json
+
+cd "$PROJ_ROOT/Ubuntu"
+packer build -except vsphere-iso -except qemu -except virtualbox-ovf -except vmware-vmx ubuntu-bionic64.json
+packer build -only qemu ubuntu-bionic64.json
+packer build -except vsphere-iso -except qemu -except virtualbox-ovf -except vmware-vmx ubuntu-xenial64.json
+packer build -only qemu ubuntu-xenial64.json
+packer build -except vsphere-iso -except qemu -except virtualbox-ovf -except vmware-vmx ubuntu-focal64.json
+packer build -only qemu ubuntu-focal64.json
