18b669f1c0f546a728c98e8e0ad889790a9a09cc
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-09-08 13:19:50 -0400

    Tweaked cleanup script
    Make sure to clean everything up including box, iso, and ovf

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-find . -type f -not -path "./venv/*" -name "*.box"
-find . -type f -not -path "./venv/*" -name "*.box" | xargs rm
-find . -type f -not -path "./venv/*" -name "*.iso"
-find . -type f -not -path "./venv/*" -name "*.iso" | xargs rm
+find . -type d -name "port"
+find . -type d -name "port" | xargs rm -rf
 find . -type d -not -path "./venv/*" -name "output-*"
 find . -type d -not -path "./venv/*" -name "output-*" | xargs rm -rf
+find . -type f -not -path "./venv/*" -name "*.box"
+find . -type f -not -path "./venv/*" -name "*.box" | xargs rm
+find . -type f -not -path "./venv/*" -name "*.iso.lock"
+find . -type f -not -path "./venv/*" -name "*.iso.lock" | xargs rm
+find . -type f -not -path "./venv/*" -name "*.iso"
+find . -type f -not -path "./venv/*" -name "*.iso" | xargs rm
+find . -type f -not -path "./venv/*" -name "*.ovf"
+find . -type f -not -path "./venv/*" -name "*.ovf" | xargs rm
+find . -type l -not -path "./venv/*" -name "output-*"
+find . -type l -not -path "./venv/*" -name "output-*" | xargs rm
